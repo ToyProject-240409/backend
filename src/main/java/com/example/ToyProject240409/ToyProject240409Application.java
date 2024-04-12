5c4c2c2deccd440ce178406a0215e4241cba9b42
dylee00 <idayeon@MacBook-Pro-2.local>
2024-04-13 01:00:20 +0900

    Initial commit

---
+++ src/main/java/com/example/ToyProject240409/ToyProject240409Application.java
@@ -0,0 +1,13 @@
+package com.example.ToyProject240409;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProject240409Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProject240409Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ToyProject240409/ToyProject240409ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToyProject240409;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject240409ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
