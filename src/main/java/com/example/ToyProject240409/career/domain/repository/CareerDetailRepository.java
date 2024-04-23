81cb8f4aa245e1fffafd63afc34c982d952034b0
dylee00 <dylee@sookmyung.ac.kr>
2024-04-24 00:36:09 +0900

    commit

--- src/main/java/com/example/ToyProject240409/career/domain/repository/CareerDetailRepository.java
+++ src/main/java/com/example/ToyProject240409/career/domain/repository/CareerDetailRepository.java
@@ -1,13 +1,21 @@
 package com.example.ToyProject240409.career.domain.repository;
 
 import com.example.ToyProject240409.career.domain.CareerDetail;
+import com.example.ToyProject240409.career.dto.CareerDetailDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CareerDetailRepository extends JpaRepository<CareerDetail,UUID> {
+    @Query("select new com.example.ToyProject240409.career.dto.CareerDetailDto(d.address, d.detailcourse, d.tel, d.notice) " +
+    "FROM CareerDetail d join d.career c " +
+    "WHERE c.careerUuid= :careerUuid ")
+    List<CareerDetailDto> findDetail(@Param("careerUuid") UUID careerUuid);
 
 
 }
