e189bc6ff5fc67e917390d577cef75c4e6bed07a
dylee00 <135154209+dylee00@users.noreply.github.com>
2024-04-24 00:39:54 +0900

    Update CareerDetailRepository.java

--- src/main/java/com/example/ToyProject240409/career/domain/repository/CareerDetailRepository.java
+++ src/main/java/com/example/ToyProject240409/career/domain/repository/CareerDetailRepository.java
@@ -8,6 +8,9 @@ import java.util.UUID;
 
 @Repository
 public interface CareerDetailRepository extends JpaRepository<CareerDetail,UUID> {
-
+    @Query("select new com.example.ToyProject240409.career.dto.CareerDetailDto(d.address, d.detailcourse, d.tel, d.notice) " +
+    "FROM CareerDetail d join d.career c " +
+    "WHERE c.careerUuid= :careerUuid ")
+    List<CareerDetailDto> findDetail(@Param("careerUuid") UUID careerUuid);
 
 }
